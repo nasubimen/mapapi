2a644e382225a260d33a178146bc1d9c5a502dc7
nasubimen <93132252+nasubimen@users.noreply.github.com>
2023-03-19 19:46:17 +0900

    mapのピン表示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,58 +3,57 @@
 <head>
     <meta charset="utf-8">
     <title>Google Maps JavaScript API</title>
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-        #map {
-            height: 500px;
-            width: 100%;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body class="antialiased">
+  @extends('layouts.header')
+  @section('content')
     <div id="map"></div>
-@php
-        // $address =array(
-        //     '滋賀県彦根市長曽根南町４７２−２',
-        //     '滋賀県長浜市南呉服町１１−２８',
-        // );
-        foreach ($items as $item) {
-            $address[] = "{{$item->address}}";
-        }
-        print_r($address);
-        $param_json = json_encode($address); //JSONエンコード
-        
-    @endphp
+    <script>
+
+    </script>
     <script>
         function initMap() {
-         
           var target = document.getElementById('map'); //マップを表示する要素を指定
-          var addresses = JSON.parse('<?php echo $param_json; ?>'); //住所を指定
           var geocoder = new google.maps.Geocoder();
           var map = new google.maps.Map(target, {
-                 center: { lat: 35.300250, lng: 136.122680 } ,
+                 center: { lat: 35.250307, lng: 136.100611} ,
                  zoom: 10
                });
                  // 各マーカーのインスタンスとInfoWindowのインスタンスを配列で保持する
         var markers = [];
         var infoWindows = [];
-               addresses.forEach(function(address) {
+        @foreach ($items as $item )
+        console.log(name);
+        var address = "{{ htmlspecialchars($item->address) }}";
         geocoder.geocode({ address: address }, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                var addressname = "{{ htmlspecialchars($item->address) }}";
+                var name = "{{ htmlspecialchars($item->name) }}";
+                var type = "{{ htmlspecialchars($item->type) }}";
+
+            var iconUrl;
+
+            if (type == 1) {
+              iconUrl = '/images/icon1.png';
+            } else {
+              iconUrl = '/images/icon2.png';
+            }
             var marker = new google.maps.Marker({
               position: results[0].geometry.location,
               map: map,
+              icon: iconUrl,
               animation: google.maps.Animation.DROP
             });
 
-                // 情報ウィンドウを表示するための内容を指定する
-                var infoWindow = new google.maps.InfoWindow({
-                    content: '<div><p>' + address + '</p></div>'
-
+            var infoWindow = new google.maps.InfoWindow({
+              content: '<h2><p>' + name + '</p></h2><div><p>' + addressname + '</p></div><a href="' + getAddressUrl({{ $item->id }}) + '">詳細を見る</a>'
             });
-            
+
+            function getAddressUrl(id) {
+              return '{{ route('address.show',$item->id) }}';
+            }
+
                 marker.addListener('click', function() {
               // 他のInfoWindowを閉じる
               infoWindows.forEach(function(infoWindow) {
@@ -71,15 +70,12 @@
             console.log('Geocode was not successful for the following reason: ' + status);
           }
         });
-      });
+        @endforeach
 
         }
         </script>
-
-
-
-
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUO3LyylA7NTT5Pzq9ROi2B1e94OAgF6E&callback=initMap" async defer></script>
+    @endsection
 </body>
 </html>
 
