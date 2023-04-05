e84c9cdad368e7f10dd088c3d28573c51c29c5ef
nasubimen <93132252+nasubimen@users.noreply.github.com>
2023-04-05 21:54:34 +0900

    編集画面コードバグ修正

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -7,7 +7,8 @@
 
 </head> --}}
 
-<form class="container mt-3" action="{{route('admin.store')}}" method="post">
+<form class="container mt-3" action="{{route('admin.update',$item->id)}}" method="post">
+  @method('PUT')
   @if ($errors->any())
   <ul>
       @foreach ($errors->all() as $error)
@@ -21,19 +22,19 @@
   <div class="form-content">
     <div class="form-group">
       <label class="label" for="name">名前</label>
-      <input type="text" class="form-control"  name="name[]" id="name" value="{{$item->name}}">
+      <input type="text" class="form-control"  name="name" id="name" value="{{$item->name}}">
     </div>
     <div class="form-group mt-2">
       <label class="label" for="address">住所</label>
-      <input type="text" class="form-control"  name="address[]" id="address" value="{{$item->address}}">
+      <input type="text" class="form-control"  name="address" id="address" value="{{$item->address}}">
     </div>
     <div class="form-group mt-2">
       <label class="label" for="url">URL</label>
-      <input type="text" class="form-control"  name="url[]" id="url" value="{{$item->url}}">
+      <input type="text" class="form-control"  name="url" id="url" value="{{$item->url}}">
     </div>
     <div class="form-group mt-2">
       <label class="label" for="type">ジャンル</label></label>
-      <select class="form-select" id="type" name="type[]">
+      <select class="form-select" id="type" name="type">
         <option value="">選択してください</option>
         <option value="1" {{"$item->type" === "1" ? "selected" : "";}}>レジャー施設</option>
         <option value="2" {{"$item->type" === "2" ? "selected" : "";}}>サービス施設</option>
@@ -42,7 +43,7 @@
     </div>
     <div class="form-group mt-2">
       <label for="detail">詳細</label>
-      <textarea class="form-control" id="detail" name="detail[]" placeholder="詳細を入力してください" rows="3">{{$item->detail}}</textarea>
+      <textarea class="form-control" id="detail" name="detail" placeholder="詳細を入力してください" rows="3">{{$item->detail}}</textarea>
     </div>
   </div>
 </div>
